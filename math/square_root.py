eea833726aa6894c9954ac29d27337689110d9d6
robertzhang <67719835+Robertation256@users.noreply.github.com>
2024-06-24 21:04:58 -0500

    Add newton's method

---
+++ math/square_root.py
@@ -0,0 +1,4 @@
+# Newton's method for caculating square root
+# for computing x = a^(0.5), simply approximate with X(n+1) = 1/2(Xn + a/Xn) with some X1 > 0
+
+#Therefore complexity for computing sqrt(n) is generally O(logn) when dealing with bounded input word length
