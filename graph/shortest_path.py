ee60fbc6665afa1e0c8f9eab51b75b07cd09f19f
robertzhang <67719835+Robertation256@users.noreply.github.com>
2024-08-24 10:08:21 -0500

    Update shortest_path.py

--- graph/shortest_path.py
+++ graph/shortest_path.py
@@ -28,22 +28,29 @@ def dijkstra_shortest_path(adj_list: dict[str, list[tuple[str, int]]], start_nod
 # Floyd–Warshall algorithm
 # for computing shortest paths between all pairs of vertices in a directed, weighted graph
 # complexity O(V^3)
-def shortest_pathes(dist: dict[int, dict[int, int]], vertices_count: int):
-    min_cost = [[float('inf') for i in range(vertices_count)] for j in range(vertices_count)]
-
-    for u in dist.keys():
-        for v in dist[u].keys():
-            min_cost[u][v] = min(min_cost[u][v], dist[u][v])
-    
+# let f(i, j, k) denote the shortest path we can get from node i to j using first k nodes as intermediate nodes
+# f(i, j, 0) is thus the minimum edge weight from i to j
+# f(i, j, k) = min(f(i, k, k-1) + f(k, j, k-1), f(i, j, k-1)) since we can either use or not use the k-th node as intermediary node
+def shortest_pathes(dist: dict[tuple[int, int], int], vertices_count: int):
+    distance_matrix = [[float('inf') for i in range(vertices_count)] for j in range(vertices_count)]
 
     for u in range(vertices_count):
         for v in range(vertices_count):
-            if u == v:
-                min_cost[u][v] = 0
-                continue
-            for k in range(vertices_count): # k the intermediary vertices that we wish to connect u and v
-                min_cost[u][v] = min(min_cost[u][v], min_cost[u][k] + min_cost[k][v])
-    return min_cost
+            if (u, v) in dist:
+                distance_matrix[u][v] = min(distance_matrix[u][v], dist[(u, v)])
+    
+
+    for k in range(vertices_count):
+        for u in range(vertices_count):
+            for v in range(vertices_count):
+                distance_matrix[u][v] = min(
+                    distance_matrix[u][v],
+                    # note that we update the previous matrix snapshot representing f(*, *, k-1) in place here
+                    distance_matrix[u][k] + distance_matrix[k][v]
+                )
+    return distance_matrix
+
+
 
 
 
